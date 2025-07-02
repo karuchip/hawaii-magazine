83b546f5d513afccacd7a8c0910591fc894c312e
Hikaru Hatakeyama <hkr.12karu@gmail.com>
2025-07-02 21:06:31 +0900

    [modified]　デプロイレイアウト調整

--- app/components/commentCreate.tsx
+++ app/components/commentCreate.tsx
@@ -19,7 +19,7 @@ const CommentCreate = ({loginUserId, postId, onCommentCreated}:commentType)=>{
 
   const [commentCount, setCommentCount] = useState(0);
   const [loading, setLoading] = useState(false)
-  const {register, handleSubmit, formState:{errors}} = useForm<FormInput>()
+  const {register, handleSubmit, setValue, formState:{errors}} = useForm<FormInput>()
 
   const onSubmit = async(data: FormInput) => {
 
@@ -44,6 +44,7 @@ const CommentCreate = ({loginUserId, postId, onCommentCreated}:commentType)=>{
 
       onCommentCreated()
       setLoading(false)
+      setValue("comment", "")
       alert(jsonData.message)
 
     }catch(error) {
