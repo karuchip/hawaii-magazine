21694919c0be9585d4eecf0f44e856ddb1b864aa
Hikaru Hatakeyama <hkr.12karu@gmail.com>
2025-07-09 22:37:28 +0900

    [add]readSigleにて、垂直水平方向に距離が10km以内の投稿取得表示機能を実装

--- app/post/readsingle/[id]/page.tsx
+++ app/post/readsingle/[id]/page.tsx
@@ -1,7 +1,7 @@
 import Form from "./form"
 import { AllItemTypes } from "@/utils/types/post"
 
-
+// 1投稿取得
 const getSingleItem = async(id: string) => {
   const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/post/readsingle/${id}`)
   const jsonData = await response.json()
@@ -9,9 +9,22 @@ const getSingleItem = async(id: string) => {
   return singleItem
 }
 
+//lat,lonを中心地とした、20km以内の投稿取得
+const getNearDistancePost = async(lat:number, lon:number) => {
+  const nearDistanceRes = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/post/nearDistance/?lat=${lat}&lon=${lon}`)
+  const nearDistanceData = await nearDistanceRes.json()
+  const nearPosts = nearDistanceData.nearPosts
+  return nearPosts
+}
+
 const ReadSingleItem = async({params}:any) => {
+
   const singleItem:AllItemTypes = await getSingleItem(params.id)
   const postId = Number(singleItem.id)
+
+  const nearPosts = singleItem.lat && singleItem.lon
+  ? await getNearDistancePost(singleItem.lat, singleItem.lon)
+  : []
 
   if(!singleItem) {
     return <div>投稿が見つかりませんでした</div>
@@ -21,7 +34,7 @@ const ReadSingleItem = async({params}:any) => {
 
     <div className="singlePostContainer">
       <div className="singlePostContent">
-        <Form singleItem={singleItem} postId={postId}/>
+        <Form singleItem={singleItem} postId={postId} nearPosts={nearPosts}/>
       </div>
     </div>
   )
